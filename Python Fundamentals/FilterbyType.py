dc0e41cf57ee450a16d219eb8ec1234753d405d8
carlos arosemena <ceag68@hotmail.com>
2017-09-05 15:42:19 -0400

    update

--- Python Fundamentals/FilterbyType.py
+++ Python Fundamentals/FilterbyType.py
@@ -1,6 +1,21 @@
+sI = 45
+mI = 100
+bI = 455
+eI = 0
+spI = -23
+sS = "Rubber baby buggy bumpers"
+mS = "Experience is simply the name we give our mistakes"
+bS = "Tell me and I forget. Teach me and I remember. Involve me and I learn."
+eS = ""
+aL = [1,7,4,21]
+mL = [3,5,7,34,3,2,113,65,8,89]
+lL = [4,34,22,68,9,13,3,5,7,9,2,12,45,923]
+eL = []
+spL = ['name','address','phone number','social security number']
+
 # Assignment: Filter by Type
 # Write a program that, given some value, tests that value for its type. Here's what you should do for each type:
-#
+
 # Integer
 # If the integer is greater than or equal to 100, print "That's a big number!" If the integer is less than 100, print
 # "That's a small number"
@@ -14,3 +29,24 @@
 # "Short list."
 #
 # Test your program using these examples:
+
+
+# set one of the above variables as the current one I'm testing
+current_tester = spI
+
+curr_type = type(current_tester)
+if isinstance(current_tester,int):
+    if current_tester >= 100:
+        print ("That's a big number!")
+    else:
+        print ("That's a small number!")
+elif curr_type is str:
+    if len(current_tester) >= 50:
+        print ("Long sentence.")
+    else:
+        print ("Short sentence.")
+elif isinstance(current_tester, list):
+    if len(current_tester) >= 10:
+        print ("Big list!")
+    else:
+        print ("Short list.")
---
+++ Python Fundamentals/ReadMeCarlosBio.txt
@@ -0,0 +1,22 @@
+My name is Carlos and I'm PM / Scrum Master originally from Panama, but joining out of Toronto,ON,Canada.
+I've always been passionate about coding. However, when I started my career in Panama, I got the chance to jump straight into PM'ing
+reason why I never had the joy to code on the job.
+
+10 years later (I started my PM career right out of Grad School in '07) and after growing and taking more and more
+complex projects I realized that that the whole "A PM doesn't need to be technical" mantra is running its course... let me be
+blunt, IT IS DYING... my estimation is that in 3-5 years there will be very little room for Non Tech PM/SM's in the software
+development biz/scene.
+
+I've been challenged with extremely complex initiatives in which technical leadership is of the essence.
+
+Not to mentioned Google and Amazon leading the Technical Project/Program Manager trend in which you are required to code
+on the interview.
+
+My goals:
+
+I have a couple of short term projects, but I prefer to show them off when I'm done rather than jinx it :P
+One thing I want to do is code along with my team, be one of the guys and not just the guy driving the timeline and asking
+for updates...  :(
+
+another dream of mine is to code plugins for JIRA / CONFLUENCE. I have really cool ideas that I know will be very helpful
+to the PM/SM  community.
